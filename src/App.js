c34a548cbfaf59492fb42f00f651d900b3b3448f
angel112 <ngllakra@gmail.com>
2019-09-24 22:17:29 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -9,14 +9,23 @@ import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import auth from "./services/authService";
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
   render() {
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
             <Route path="/movies/:id" component={MovieForm} />
@@ -24,6 +33,7 @@ class App extends Component {
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
             <Route path="/login" component={LoginForm}></Route>
+            <Route path="/logout" component={Logout}></Route>
             <Route path="/register" component={RegisterForm}></Route>
             <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/movies" />
